e588fd5a5023e8e676c1f8511a52bf7692c0b758
R Su <74230400+rooyi@users.noreply.github.com>
2023-11-30 02:03:00 -0800

    Create sum.js

---
+++ sumdemo/sum.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const diceNumberInput = document.getElementById('diceNumber');
+  const diceValueDisplay = document.getElementById('diceValue');
+  const diceContainer = document.getElementById('diceContainer');
+  const individualExpectedValue = document.getElementById('individualExpectedValue');
+  const totalExpectedValue = document.getElementById('totalExpectedValue');
+
+  diceNumberInput.addEventListener('input', updateDiceNumber);
+
+  function updateDiceNumber() {
+    const numDice = diceNumberInput.value;
+    diceValueDisplay.textContent = numDice;
+    displayDice(numDice);
+    calculateExpectedValue(numDice);
+  }
+
+  function displayDice(numDice) {
+    diceContainer.innerHTML = '';
+
+    for (let i = 1; i <= numDice; i++) {
+      const diceImg = document.createElement('img');
+      diceImg.src = PLACE HOLDER; // IMAGE REPLACE
+      diceContainer.appendChild(diceImg);
+    }
+  }
+
+  function calculateExpectedValue(numDice) {
+    const individualValue = 3.5; 
+    const totalValue = numDice * individualValue;
+
+    individualExpectedValue.textContent = `Individual Expected Value: ${individualValue}`;
+    totalExpectedValue.textContent = `Total Expected Value: ${totalValue}`;
+  }
+
+  // Initial setup
+  updateDiceNumber();
+});
