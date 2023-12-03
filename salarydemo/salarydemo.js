5cf1c9326e8000d1c6dba9444c9440daf5b112a3
R Su <74230400+rooyi@users.noreply.github.com>
2023-12-03 15:57:07 -0800

    Update salarydemo.js

--- salarydemo/salarydemo.js
+++ salarydemo/salarydemo.js
@@ -1,21 +1,9 @@
 function generateRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function createPersonElement(personIndex, hoursWorked) {
-    const personContainer = document.getElementById('people-container');
-    const personElement = document.createElement('div');
-    personElement.classList.add('person');
-    personElement.innerHTML = `
-        <img src="person.png" alt="Person ${personIndex + 1}">
-        <div class="hours-text">Hours: <span>${hoursWorked}</span></div>
-    `;
-    personContainer.appendChild(personElement);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function clearPeopleContainer() {
-    const personContainer = document.getElementById('people-container');
-    personContainer.innerHTML = '';
+    document.getElementById('people-container').innerHTML = '';
 }
 
 function calculateSalary() {
@@ -55,4 +43,16 @@ function calculateSalary() {
     resultElement.appendChild(expectationDisplay);
 }
 
-
+function createPersonElement(personIndex, hoursWorked, earnings) {
+    const personContainer = document.getElementById('people-container');
+    const personElement = document.createElement('div');
+    personElement.classList.add('person');
+    personElement.innerHTML = `
+        <img src="person.png" alt="Person ${personIndex + 1}">
+        <div class="hours-text">
+            Hours: <span>${hoursWorked}</span><br>
+            $: <span>${earnings.toFixed(2)}</span>
+        </div>
+    `;
+    personContainer.appendChild(personElement);
+}
