54645c3198a6becfc2fe5e74d25bd5f02a433055
R Su <74230400+rooyi@users.noreply.github.com>
2023-12-03 02:43:28 -0800

    Update salarydemo.js

--- salarydemo/salarydemo.js
+++ salarydemo/salarydemo.js
@@ -45,11 +45,8 @@ function generateRandomNumber(min, max) {
 }
 
 function createPersonElement(personIndex, hoursWorked) {
-    const personContainer = document.getElementById('people-container');
-    const personElement = document.createElement('div');
-    personElement.classList.add('person');
-    personElement.innerHTML = `<img src="person.png" alt="Person ${personIndex + 1}"><br>${hoursWorked} hours`;
-    personContainer.appendChild(personElement);
+    const hoursSpan = document.getElementById(`hours${personIndex + 1}`);
+    hoursSpan.textContent = hoursWorked;
 }
 
 function calculateSalary() {
