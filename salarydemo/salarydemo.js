32659ef325f100efd5ca5790b6dfb4a93a64bba5
R Su <74230400+rooyi@users.noreply.github.com>
2023-12-03 02:53:40 -0800

    Update salarydemo.js

--- salarydemo/salarydemo.js
+++ salarydemo/salarydemo.js
@@ -1,3 +1,7 @@
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function clearPeopleContainer() {
     const peopleContainer = document.getElementById('people-container');
     peopleContainer.innerHTML = '';
@@ -40,35 +44,3 @@ function calculateSalary() {
     resultElement.appendChild(bonusDisplay);
 }
 
-function generateRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function createPersonElement(personIndex, hoursWorked) {
-    const hoursSpan = document.getElementById(`hours${personIndex + 1}`);
-    hoursSpan.textContent = hoursWorked;
-}
-
-function calculateSalary() {
-    const hourlyRate = parseFloat(document.getElementById('hourlyRate').value) || 0;
-    const bonusAmount = parseFloat(document.getElementById('bonusAmount').value) || 0;
-
-    let totalSalary = 0;
-
-    for (let i = 0; i < 10; i++) {
-        const hoursWorked = generateRandomNumber(1, 10);
-        totalSalary += hourlyRate * hoursWorked + bonusAmount;
-        createPersonElement(i, hoursWorked);
-    }
-
-    const resultElement = document.getElementById('result');
-    resultElement.textContent = `Expectation E[aX + b] = ${totalSalary.toFixed(2)}`;
-
-    const hourlyRateDisplay = document.createElement('div');
-    hourlyRateDisplay.textContent = `Hourly Rate: ${hourlyRate.toFixed(2)}`;
-    resultElement.appendChild(hourlyRateDisplay);
-
-    const bonusDisplay = document.createElement('div');
-    bonusDisplay.textContent = `Bonus: ${bonusAmount.toFixed(2)}`;
-    resultElement.appendChild(bonusDisplay);
-}
