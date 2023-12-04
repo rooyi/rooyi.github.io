230e825d10468327f0a92ed0f8af21936d8e0237
R Su <74230400+rooyi@users.noreply.github.com>
2023-12-03 17:14:40 -0800

    Update salarycount.js

--- salarycount/salarycount.js
+++ salarycount/salarycount.js
@@ -7,5 +7,5 @@ function calculateHoursWorked() {
 
     document.getElementById('expectedValueValue').textContent = (totalHours / 5).toFixed(2);
     document.getElementById('equationValue').textContent =
-        `${hourlyRate} x ${totalHours} / 5 + ${bonusAmount} = ${expectationPerPerson.toFixed(2)}`;
+        `${hourlyRate} x ${totalHours / 5} + ${bonusAmount} = ${expectationPerPerson.toFixed(2)}`;
 }
