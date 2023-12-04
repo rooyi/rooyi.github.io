a98c8643fb812887449b04a5f66693b8b1639465
R Su <74230400+rooyi@users.noreply.github.com>
2023-12-03 16:55:03 -0800

    Create salarycount.js

---
+++ salarycount/salarycount.js
@@ -0,0 +1,11 @@
+function calculateHoursWorked() {
+    const totalHours = parseFloat(document.getElementById('totalHours').value) || 0;
+    const hourlyRate = parseFloat(document.getElementById('hourlyRate').value) || 0;
+    const bonusAmount = parseFloat(document.getElementById('bonusAmount').value) || 0;
+
+    const expectedValue = hourlyRate * totalHours + bonusAmount;
+
+    document.getElementById('expectedValueValue').textContent = expectedValue.toFixed(2);
+    document.getElementById('equationValue').textContent =
+        `${hourlyRate} x ${totalHours} + ${bonusAmount} = ${expectedValue.toFixed(2)}`;
+}
