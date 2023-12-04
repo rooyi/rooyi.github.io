80844f1e2aab87a9f84b6e141f4dbb6af9f42bd1
R Su <74230400+rooyi@users.noreply.github.com>
2023-12-03 17:20:34 -0800

    Update salarycount.js

--- salarycount/salarycount.js
+++ salarycount/salarycount.js
@@ -3,9 +3,9 @@ function calculateHoursWorked() {
     const hourlyRate = parseFloat(document.getElementById('hourlyRate').value) || 0;
     const bonusAmount = parseFloat(document.getElementById('bonusAmount').value) || 0;
 
-    const expectationPerPerson = hourlyRate*(totalHours / 5) + bonusAmount;
+    const expectationPerPerson = (hourlyRate * totalHours / 5) + bonusAmount;
 
     document.getElementById('expectedValueValue').textContent = (totalHours / 5).toFixed(2);
     document.getElementById('equationValue').textContent =
-        `${hourlyRate} x ${totalHours / 5} + ${bonusAmount} = ${expectationPerPerson.toFixed(2)}`;
+        `${hourlyRate.toFixed(2)} x ${totalHours} / 5 + ${bonusAmount.toFixed(2)} = ${expectationPerPerson.toFixed(2)}`;
 }
